c4b3d6506a9fb3cfdac4eb5ca4a15255046e0ce0
Facundo Martinez Correa <fnmartinez88@gmail.com>
2012-03-16 10:31:26 -0300

    proto fifos working

--- rcvmessage.c
+++ rcvmessage.c
@@ -7,6 +7,8 @@
 
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 
 //#define MSGSIZ 63
@@ -15,48 +17,104 @@ char *fifo = "fifo";
 
 main(int argc, char ** argv)
 {
-	int fd;
-	char msgbuf[];
+	int fdIn, fdOut;
+	void *msgbuf;
 
 	if(mkfifo(fifo, 0666) == -1)
 	{
 		if(errno != EEXIST)
 		{
-			printf("receiver: mkfifo");
+			perror("receiver: fifoIn");
 		}
 	}
 
-	if((fd = open(fifo, O_RDWR)) < 0)
+
+	if((fdIn = open(fifo, O_RDONLY)) < 0)
 	{
-		printf("fifo open failed");
+		perror("fifo in open failed");
 
 	}
 
 	for(;;)
 	{
+		int size;
 		int count;
-		msgbuf = malloc(sizeof(int));
-
-		if((count = read(fd, msgbuf, sizeof(int))) < 0)
+		if(read(fdIn, &size, sizeof(int)) == -1)
 		{
-			printf("message read failed");
+			perror("failed to read the size");
 		}
-		else if (count > 0)
+		else
 		{
-			int opt = atoi(msgbuf);
-			switch(opt){
-			case 0:
-				exit(0);
-				break;
-			case 1:
-				char msg[5];
-				read();
-				break;
+			msgbuf = malloc(size);
+			if((count = read(fdIn, msgbuf, size)) < 0)
+			{
+				printf("message read failed");
+			}
+			else if (count > 0)
+			{
+				//printf("%c\n", msgbuf[0]);
+				int opt, msgSize, i;
+				float pi;
+				char *msg;
+				memcpy(&opt, msgbuf, sizeof(int));
+				void * aux = msgbuf;
+				aux+=sizeof(int);
+				printf("Opción: %d\n", opt);
+				switch(opt){
+				case 0:
+					printf("se cierra\n");
+					close(fdIn);
+					unlink(fifo);
+					exit(0);
+					break;
+				case 1:
+					memcpy(&msgSize, aux, sizeof(int));
+					msg = malloc(msgSize);
+					aux+=sizeof(int);
+					memcpy(msg, aux, msgSize);
+					printf("Mensaje: %s\n", msg);
+					free(msg);
+					break;
+				case 2:
+					memcpy(&i, aux, sizeof(int));
+					printf("Mensaje: %d\n", i);
+					break;
+				case 3:
+					memcpy(&pi, aux, sizeof(float));
+					printf("Mensaje: %f\n", pi);
+					break;
+				case 4:
+					printf("PING!\n");
+					memcpy(&msgSize, aux, sizeof(int));
+					msg = malloc(msgSize);
+					aux+=sizeof(int);
+					memcpy(msg, aux, msgSize);
+					printf("PONG en %s\n", msg);
+					int nwrite;
+					char pong[]="PONG";
+					int pongSize = strlen(pong);
+					if((fdOut = open(msg, O_WRONLY)) < 0)
+					{
+						perror("fifo out failed open");
+					}
+					if((nwrite = write(fdOut, &pongSize, sizeof(int))) == -1)
+					{
+						perror("pong size write failed");
+					}
+					if((nwrite = write(fdOut, pong, pongSize)) == -1)
+					{
+						perror("pong write failed");
+					}
+					free(msg);
+					close(fdOut);
+					unlink(msg);
+					break;
+				}
+				free(msgbuf);
 			}
 		}
 
-
-		printf("message received:%s\n", msgbuf);
+		//printf("message received:%s\n", msgbuf);
 	}
 
 }
--- sendmessage.c
+++ sendmessage.c
@@ -7,8 +7,12 @@
 
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 
+#define TRUE 1
+#define FALSE 0
 
 typedef struct message {
 	int type;
@@ -19,13 +23,14 @@ typedef struct message {
 	};
 } message;
 
-char *fifo = "fifo";
+char *fifoOut = "fifo";
 
 main (int argc, char **argv)
 {
-	int fd, j, nwrite;
-	char msgbuf[];
-
+	char * fifoIn = tempnam(".", NULL);
+	int fdIn, fdOut, j, nwrite;
+	void * msgbuf;
+	int pingFlag = FALSE;
 
 	if(argc < 2)
 	{
@@ -33,64 +38,111 @@ main (int argc, char **argv)
 		exit(1);
 	}
 
-
-	if((fd = open(fifo, O_WRONLY | O_NONBLOCK)) < 0)
+	if(mkfifo(fifoIn, 0666) == -1)
 	{
-		printf("fifo open failed");
+		perror("fifo in creation failed");
+	}
+	if((fdOut = open(fifoOut, O_WRONLY )) < 0)
+	{
+		perror("fifo out open failed");
 	}
 
-
+	if((fdIn = open(fifoIn, O_RDONLY | O_NONBLOCK)) < 0)
+	{
+		perror("fifo in open failed");
+	}
 
 	for( j = 1; j < argc; j++)
 	{
 		int opt = atoi(argv[j]);
 
-		if(opt < 0 || opt > 3)
+		if(opt < 0 || opt > 4)
 		{
 			printf("Option not available");
 			exit(1);
 		}
 
 		int size;
+		char msg[] = "hola";
+		int msgSize = strlen(msg)+1;
 
 		switch(opt)
 		{
 		case 0:
-			size =2*sizeof(char);
+			size =sizeof(int);
 			msgbuf = malloc(size);
-			msgbuf[0] = "0";
-			msgbuf[1] = ";";
+			memcpy(msgbuf, &opt, size);
 			break;
 		case 1:
-			size=7*sizeof(char);
+			size=2*sizeof(int)+msgSize;
 			msgbuf = malloc(size);
-			msgbuf[0] = "1;";
-			msgbuf[2] = "hola;";
+			memcpy(msgbuf, &opt, sizeof(int));
+			memcpy(msgbuf+sizeof(int), &msgSize, sizeof(int));
+			memcpy(msgbuf+2*sizeof(int), msg, msgSize);
 			break;
 		case 2:
-			size = 3*sizeof(char) + sizeof(int);
+			size = 2*sizeof(int);
 			msgbuf = malloc(size);
-			msgbuf[0] = "2;";
+			memcpy(msgbuf, &opt, sizeof(int));
 			int i = 1024;
-			memcpy(((void*)msgbuf+2), &i, sizeof(int));
-			msgbuf[2+sizeof(int)] = ";";
+			memcpy(msgbuf+sizeof(int), &i, sizeof(int));
 			break;
 		case 3:
-			size =3*sizeof(char) + sizeof(float);
+			size =sizeof(int) + sizeof(float);
 			msgbuf = malloc(size);
-			msgbuf[0] = "3;";
-			float pi = 3.14;
-			memcpy(((void*)msgbuf+2), &pi, sizeof(float));
-			msgbuf[2+sizeof(float)] = ";";
+			memcpy(msgbuf, &opt, sizeof(int));
+			float pi = 3.141592;
+			memcpy(msgbuf+sizeof(int), &pi, sizeof(float));
+			break;
+		case 4:
+			size = 2*sizeof(int) + strlen(fifoIn);
+			msgbuf = malloc(size);
+			memcpy(msgbuf, &opt, sizeof(int));
+			int nameSize= strlen(fifoIn);
+			memcpy(msgbuf+sizeof(int), &nameSize, sizeof(int));
+			memcpy(msgbuf+2*sizeof(int), fifoIn, nameSize);
+			pingFlag = TRUE;
 			break;
 		}
 
-		if((nwrite = write(fd, msgbuf, size)) == -1)
+		if((nwrite = write(fdOut, &size, sizeof(int))) == -1)
 		{
-			printf("message write failed");
+			perror("size write failed");
+		}
+		if((nwrite = write(fdOut, msgbuf, size)) == -1)
+		{
+			perror("message write failed");
+		}
+
+		while(pingFlag)
+		{
+			char * pong;
+			int pongSize;
+			int nread;
+			if((nread = read(fdIn, &pongSize, sizeof(int))) == -1)
+			{
+				perror("reading pongSize");
+			}
+			else
+			{
+				pong = malloc(pongSize);
+				if((nread = read(fdIn, pong, pongSize)) < 0)
+				{
+					perror("reading PONG");
+				}
+				else if (nread > 0)
+				{
+					printf("%s\n", pong);
+					pingFlag = FALSE;
+				}
+				free(pong);
+
+			}
 		}
 	}
 
+	close(fdIn);
+	unlink(fifoIn);
 	exit(0);
 }
 
