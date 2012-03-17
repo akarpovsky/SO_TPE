006099e3ed7c1f69558d1d90972a3ab248e11fe4
Alan Karpovsky <akarpovs@alu.itba.edu.ar>
2012-03-16 22:16:39 -0300

    Fixed some bad implementations of malloc. User loading working fine.

--- IO/io.c
+++ IO/io.c
@@ -6,15 +6,22 @@
 
 #define BUFFER_SIZE 100
 
+/*
+* function loadUser
+*
+* Alloc memory for a user_t and Loads a specific user
+* fiven the path of the saved one.
+*
+* @path: Path of the saved user file 
+*/
 User loadUser(char * path){
 	
 	FILE * file;
-	char * auxString;
-	int * auxInt;
-	User auxUser;
-	char aux[BUFFER_SIZE];
+	
+
+	User auxUser; // Will store the loaded user
+	char aux[BUFFER_SIZE]; // For reading the file line by line
 	int dim, num, auxNum;
-	int i;
 	
 	auxUser = malloc(sizeof(user));
 	
@@ -33,27 +40,23 @@ User loadUser(char * path){
 	/* CARGO USER */
 	fgets(aux,BUFFER_SIZE,file);
 	dim = strlen(aux);
-	auxString  =  malloc(sizeof(dim));
-	if(auxString == NULL){
+	auxUser->user  = (char *) malloc(sizeof(dim));
+	if(auxUser->user == NULL){
 		perror("Insufficient memory\n");
 		exit(EXIT_FAILURE);
 	}
-	auxUser -> user = auxString;
 	strcpy(auxUser->user,aux);
 	auxUser->user[dim-1] = '\0';
-//	printf("%s\n",auxUser->user);
 	
 	
 	/* CARGO PASSWORD */
 	fgets(aux,BUFFER_SIZE,file);
 	dim = strlen(aux);
-	printf("%d\n",dim);
-	auxString  =  malloc(sizeof(dim));
-	if(auxString == NULL){
+	auxUser->pass  = (char *)  malloc(sizeof(dim));
+	if(auxUser->pass == NULL){
 		perror("Insufficient memory\n");
 		exit(EXIT_FAILURE);
 	}
-	auxUser -> pass = auxString;
 	strcpy(auxUser->pass,aux);
 	auxUser->pass[dim-1] = '\0';
 	
@@ -62,15 +65,14 @@ User loadUser(char * path){
 	fgets(aux,BUFFER_SIZE,file);
 	num = atoi(aux);
 	auxUser->leagues = num;
-	
-	auxInt= malloc(sizeof(num) / sizeof(int));
-	if(auxInt == NULL){
+	auxUser->leagueIDs = malloc((num-1) * sizeof(int));
+	if(auxUser->leagueIDs == NULL){
 		perror("Insufficient memory\n");
 		exit(EXIT_FAILURE);
 	}
-	auxUser->leagueIDs = auxInt;
 
 	/* Cargo IDs en el vector de IDs anteriormente malloqueado */
+	int i;
 	for(i = 0; i < num; i++){
 		fgets(aux,BUFFER_SIZE,file);
 		auxNum = atoi(aux);
@@ -83,9 +85,7 @@ User loadUser(char * path){
 
 int main(void){
 	
-	User vicky = loadUser("../res/users/Vicky.user");
-	printf("%s\n",vicky->user);
-	
+	User vicky = loadUser("../res/users/Vicky.user");	
 	printf("<LOG - io.c>\n\tCargue al usuario: %s\n\tTene password: %s\n\tCantidad de ligas: %d\n<end>\n", vicky->user, vicky->pass, vicky->leagues);
 	return 0;
 }
