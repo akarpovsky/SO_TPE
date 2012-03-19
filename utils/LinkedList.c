e55456bf99da6b607ce34ef2f756c80b3694c2d3
Alan Karpovsky <akarpovs@alu.itba.edu.ar>
2012-03-19 00:33:28 -0300

    Fixed malloc sizeof()

--- utils/LinkedList.c
+++ utils/LinkedList.c
@@ -14,12 +14,20 @@ Element AddToList(void *item, List list)
 {
     //check inputs
   //  assert(item!=NULL); assert(list!=NULL);
-    
+    if(item == NULL || list == NULL){
+		printf("<LOG - LinkedList.c>\n\tNull pointer.\n<end>\n");
+		exit(EXIT_FAILURE);
+	}
+
 	//Create generic element to hold item ptr
     Element NewEl;
-    NewEl = (Element)malloc(sizeof(NewEl));  //create generic element
- 
- 	//  assert(NewEl != NULL);
+    NewEl = (Element)malloc(sizeof(lelement));  //create generic element
+
+	if(NewEl == NULL){
+		printf("<LOG - LinkedList.c>\n\tInsufficient memory.\n<end>\n");
+		exit(EXIT_FAILURE);
+	}
+
     list->NumEl = list->NumEl + 1;
     NewEl->data = item;
     if (list->NumEl == 1)
