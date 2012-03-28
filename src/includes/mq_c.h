401aa930414307a478f3a33f8e8655848930d144
Alan Karpovsky <akarpovs@alu.itba.edu.ar>
2012-03-28 00:37:31 -0300

    Socket and Message_queue working :)

---
+++ src/includes/mq_c.h
@@ -0,0 +1,11 @@
+#ifndef MQ_C_H
+#define MQ_C_H
+
+#define MAXSIZE	128
+#define FTOK_ID 1
+#define FTOK_FILE "/tmp/msgq"
+#define MAIN_SERVER_PRIORITY 1
+#define MAX_STRING 200
+#define SUCCESSFUL 1
+
+#endif
