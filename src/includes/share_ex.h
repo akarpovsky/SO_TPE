69d9ca188a7a13be43fd2c596c24dee537953404
Alan Karpovsky <akarpovs@alu.itba.edu.ar>
2012-04-01 20:44:18 -0300

    Fixed FIFOs, now working .. Nike, just do it

--- src/includes/share_ex.h
+++ src/includes/share_ex.h
@@ -11,7 +11,8 @@
 #define SHMKEY (key_t) 0x10
 #define SERVER_SEM "/server"
 
-#define SERVER_BUF_SIZE (10 * BUFSIZ)
+#define MAX_CLIENTS 10
+#define SERVER_BUF_SIZE (MAX_CLIENTS * BUFSIZ)
 #define CLIENT_BUF_SIZE BUFSIZ;
 
 typedef sem_t * Sem;
@@ -19,6 +20,7 @@ typedef sem_t * Sem;
 typedef struct channel_t {
 	Sem memory_lock_sem; // Locks the shared memory so no data is corrupted
 	Sem new_msg_sem; // Signals the client or the server saying: "You have data to read"
+	void * buffer; // Where client will read and write data
 } channel_t;
 
 typedef channel_t * Channel;
--- src/transport/shmm/shmm_s.c
+++ src/transport/shmm/shmm_s.c
@@ -5,19 +5,79 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
-#include <sys/ipc.h> 
+#include <assert.h>
+#include <syslog.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 #include <sys/shm.h>
+#include <errno.h>
+
+#include "../includes/share_ex.h"
 
 #include "../../includes/message.h"
 #include "../../utils/LinkedList.h"
-#include "../../utils/hashmap.h"
 #include "../../includes/defines.h"
 #include "../../includes/transport_s.h"
+
+int fdShMem; // Shared memory handle file descriptor
+int cantClients = 0;
+void * bufferShMem; // Where shared data will be!
+
+
+/**
+* Allocates a shared memory segment.
+*
+* @param n Size (in bytes) of chunk to allocate.
+* @return Id of shared memory chunk.
+*/
+int AllocateSharedMemory(key_t key, int n)
+{
+	assert(n > 0); 
+	int shmid;
+	 if ((shmid = shmget(key, n, 0644 | IPC_CREAT )) == -1) {
+        perror("shmget");
+        exit(1);
+    }
+
+    return shmid;
+}
+
+/**
+* Maps a shared memory segment onto our address space.
+*
+* @param id Shared memory block to map.
+* @return Address of mapped block.
+*/
+
+void * MapSharedMemory(int id)
+{
+	void * addr = shmat(id, (void *)0, 0);
+    if (addr == (char *)(-1)) {
+        perror("shmat");
+        exit(1);
+    }
+
+    return addr;
+}
+
+void down(sem_t * sem){
+	sem_wait(sem);
+	return ;
+}
+
+void up(sem_t * sem){
+	sem_post(sem);
+	return ;
+}
 
 
 void closeServer(char * server_path){
@@ -37,13 +97,71 @@ void sigint(){
 
 void uplink(void){
 
+	Sem server_sem; // Pointer to server semaphore
+	sem_unlink(SERVER_SEM); // Just in case server_sem remains open
+
+	server_sem = sem_open(SERVER_SEM, O_CREAT | O_EXCL, 0644, 0);
+	if(server_sem == SEM_FAILED)
+    {
+      perror("Unable to create semaphore");
+      sem_unlink(SERVER_SEM);
+      exit(EXIT_FAILURE);
+    }
+
+    fdShMem = AllocateSharedMemory(SHMKEY, SERVER_BUF_SIZE);
+	bufferShMem = MapSharedMemory(fdShMem);
+	ftruncate(fdShMem, SERVER_BUF_SIZE);
+	printf("Created and mapped shared memory in FD %d ; SHMM size = %d ; buffer = %d\n", fdShMem, BUFSIZ, (int) bufferShMem);
+	printf("Server finished upLink() - Server ready!\n");
+
 }
 
 Channel createChannel(Msg_t msg)
 {
-	Channel ch = malloc(sizeof(channel_t));
+	if(msg->type == CONTACT){
+		
+		if(++cantClients <= MAX_CLIENTS){
 
-	return ch;
+			Channel ch = malloc(sizeof(channel_t));
+			char[20] sem_name;
+			sprintf(sem_name, "/client_sem_%d", cantClients);
+
+			Sem client_sem; // Pointer to client semaphore
+			sem_unlink(sem_name); // Just in case client_sem remains open
+
+			client_sem = sem_open(sem_name, O_CREAT | O_EXCL, 0644, 0);
+			if(client_sem == SEM_FAILED)
+	    	{
+		      perror("Unable to create semaphore");
+		      sem_unlink(sem_name);
+		      exit(EXIT_FAILURE);
+		    }
+
+	    	Sem server_sem;
+			server_sem = sem_open(SERVER, 0, 0644, 0); /* Open a preexisting semaphore. */
+			if(server_sem == SEM_FAILED)
+		    {
+		      perror("Unable to create semaphore");
+		      sem_unlink(SERVER_SEM);
+		      exit(EXIT_FAILURE);
+		    }
+
+		    ch->new_msg_sem = client_sem;
+		    ch->memory_lock_sem = server_sem;
+			printf("Created \"%s\" and filled sem channel data\n", sem_name);
+		    
+		    ch->buffer = bufferShMem+((cantClients-1)*BUFSIZ);
+
+		    printf("Now client has a unique offset inside the shared memory space: %d\n", (cantClients-1)*BUFSIZ);
+
+			return ch;
+
+		}else{
+			return NULL; // No more slots for new clients
+		}
+	}
+
+	return NULL;
 }
 
 Msg_s establishChannel(Channel ch)
