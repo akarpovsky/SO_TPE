5de5ea846ebb40690246ae3719d6ce43d2a0af6e
Facundo Martinez Correa <fnmartinez88@gmail.com>
2012-04-09 02:13:30 -0300

    things

--- src/IO/io.c
+++ src/IO/io.c
@@ -1143,10 +1143,10 @@ void updatePlayers(List l){
 
 void dumpMatch(char * path){
 
-	char string[200];
+	char string[];
 	
-	sprintf("mv %s %s",string,path,MATCHES_DUMP);
-	
+	sprintf(string, "mv -n %s %s", path, MATCHES_DUMP);
+	system(string);
 	//exec(string);
 	
 }
